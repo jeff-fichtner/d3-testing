c6fee4fe32b9b181f6b352ab1fcace413b63ae7d
Jeff Fichtner <jeff.fichtner@gmail.com>
2016-10-16 16:26:45 -0500

    Running

--- public/js/matter_chains.js
+++ public/js/matter_chains.js
@@ -1,4 +1,4 @@
-(function() {
+function Start() {
 
     var World = Matter.World,
         Bodies = Matter.Bodies,
@@ -11,36 +11,36 @@
         var engine = demo.engine,
             world = engine.world,
             group = Body.nextGroup(true);
-         
+
         var ropeA = Composites.stack(200, 100, 5, 2, 10, 10, function(x, y) {
             return Bodies.rectangle(x, y, 50, 20, { collisionFilter: { group: group } });
         });
-        
+
         Composites.chain(ropeA, 0.5, 0, -0.5, 0, { stiffness: 0.8, length: 2 });
-        Composite.add(ropeA, Constraint.create({ 
+        Composite.add(ropeA, Constraint.create({
             bodyB: ropeA.bodies[0],
             pointB: { x: -25, y: 0 },
             pointA: { x: 200, y: 100 },
             stiffness: 0.5
         }));
-        
+
         World.add(world, ropeA);
-        
+
         group = Body.nextGroup(true);
-        
+
         var ropeB = Composites.stack(500, 100, 5, 2, 10, 10, function(x, y) {
             return Bodies.circle(x, y, 20, { collisionFilter: { group: group } });
         });
-        
+
         Composites.chain(ropeB, 0.5, 0, -0.5, 0, { stiffness: 0.8, length: 2 });
-        Composite.add(ropeB, Constraint.create({ 
+        Composite.add(ropeB, Constraint.create({
             bodyB: ropeB.bodies[0],
             pointB: { x: -20, y: 0 },
             pointA: { x: 500, y: 100 },
             stiffness: 0.5
         }));
-        
+
         World.add(world, ropeB);
     };
 
-})();
+}
