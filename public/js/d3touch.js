e60ec7df9c9261dbbdf843668a447044518228e6
Jeff Fichtner <jff2501@gmail.com>
2016-10-15 21:11:06 -0500

    Interact with mods

--- public/js/d3touch.js
+++ public/js/d3touch.js
@@ -1,74 +1,86 @@
-// $(function() {
+// target elements with the "draggable" class
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    },
+    // enable autoScroll
+    autoScroll: true,
 
-  // function touchDemo1() {
-    // d3.select("#buttons").html("")
-    // .style("pointer-events", "none")
-    // .append("div").attr("id", "touchStatus")
-    //   .append("p")
-    //   .append("ol")
-    //   .append("li");
-      // .html("Touch the screen with a touch interface to get the current touch positions using d3.touches");
-    
-  //   d3.select("svg").on("touchstart", touchStatus);
-  //   d3.select("svg").on("touchmove", touchStatus);
-    
-  //   function touchStatus() {
-  //     d3.event.preventDefault();
-  //     d3.event.stopPropagation();
-  //     d = d3.touches(this);
-  //     d3.select("#touchStatus")
-		//     .select("ol")
-		//     .selectAll("li")
-		//     .data(d)
-		//     .enter()
-		//     .append("li");
-      
-  //     d3.select("#touchStatus")
-	 //      .select("ol")
-	 //      .selectAll("li")
-	 //      .data(d)
-	 //      .exit()
-	 //      .remove();
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+    // call this function on every dragend event
+    // onend: function (event) {
+      // var textEl = event.target.querySelector('p');
 
-  //     d3.select("#touchStatus")
-	 //      .select("ol")
-	 //      .selectAll("li")
-	 //      .html(function(d) {return d});
-      
-  //   }
+      // textEl && (textEl.textContent =
+      //   'moved a distance of '
+      //   + (Math.sqrt(event.dx * event.dx +
+      //                event.dy * event.dy)|0) + 'px');
+    // }
+  });
 
-  // }
+  function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-var dragndrop = function() {
-	var myX = '';
-	var myY = '';
-	var whichArt = '';
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
 
-	// function resetZ() {
-	// 	var elements = document.querySelectorAll('img');
-	// 	for (var i = elements.length - 1; i >= 0; i--) {
-	// 		elements[i].style.zIndex = 5;
-	// 	};
-	// }
+    // update the position attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
 
-	function touchStart(e) {
-		e.preventDefault();
-		var whichArt = e.target;
-		var touch = e.touches[0];
-		var moveOffsetX = whichArt.offsetLeft - touch.pageX;
-		var moveOffsetY = whichArt.offsetTop - touch.pageY;
-		// resetZ();
-		// whichArt.style.zIndex = 10;
+  // this is used later in the resizing and gesture demos
+  window.dragMoveListener = dragMoveListener;
 
-		whichArt.addEventListener('touchmove', function() {
-			var positionX = touch.pageX + moveOffsetX;
-			var positionY = touch.pageY + moveOffsetY;
-			whichArt.style.left = positionX + 'px';
-			whichArt.style.top = positionY + 'px';
-		}, false);
-	}
-	document.getElementById('rect').addEventListener('touchstart', touchStart, false);
-}
+  /* The dragging code for '.draggable' from the demo above
+ * applies to this demo as well so it doesn't have to be repeated. */
 
+// enable draggables to be dropped into this
+interact('.dropzone').dropzone({
+  // only accept elements matching this CSS selector
+  accept: '#yes-drop',
+  // Require a 75% element overlap for a drop to be possible
+  overlap: 1.0,
 
-// });
+  // listen for drop related events:
+
+  ondropactivate: function (event) {
+    // add active dropzone feedback
+    event.target.classList.add('drop-active');
+  },
+  ondragenter: function (event) {
+    var draggableElement = event.relatedTarget,
+        dropzoneElement = event.target;
+
+    // feedback the possibility of a drop
+    dropzoneElement.classList.add('drop-target');
+    draggableElement.classList.add('can-drop');
+    draggableElement.textContent = 'Dragged in';
+  },
+  ondragleave: function (event) {
+    // remove the drop feedback style
+    event.target.classList.remove('drop-target');
+    event.relatedTarget.classList.remove('can-drop');
+    event.relatedTarget.textContent = 'Dragged out';
+  },
+  ondrop: function (event) {
+    event.relatedTarget.textContent = 'Dropped';
+  },
+  ondropdeactivate: function (event) {
+    // remove active dropzone feedback
+    event.target.classList.remove('drop-active');
+    event.target.classList.remove('drop-target');
+  }
+});
