9e69410213ca958c438d95e15ab3d82f220c2c97
Thuy <email": null>
2016-10-19 10:20:15 -0500

    Add svg block

--- public/js/dropzone_render.js
+++ public/js/dropzone_render.js
@@ -1,4 +1,4 @@
 function dropzoneRender() {
-	$('body').html("<div id=\"outer-dropzone\"><span id=\"sentenceBegin\" class=\"float-left\" ></span><div id=\"inner-dropzone\" class=\"dropzone inline float-left\"></div><span id=\"sentenceEnd\" class=\"float-left\"></span></div><div id=\"word-text\" class=\"draggable drag-drop yes-drop\"></div>")
+	$('body').html("<svg xmlns=\"http://www.w3.org/2000/svg\"xmlns:xlink=\"http://www.w3.org/1999/xlink\"></svg><div id=\"outer-dropzone\"><span id=\"sentenceBegin\" class=\"float-left\" ></span><div id=\"inner-dropzone\" class=\"dropzone inline float-left\"></div><span id=\"sentenceEnd\" class=\"float-left\"></span></div><div id=\"word-text\" class=\"draggable drag-drop yes-drop\"></div>")
 }
-  
+
