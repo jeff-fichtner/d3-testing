bba2c9760690f39b824cc40fecc54a35552deac8
Thuy <email": null>
2016-10-19 10:24:31 -0500

    Fix dropzone renders

--- public/js/dropzone_render.js
+++ public/js/dropzone_render.js
@@ -1,4 +1,6 @@
 function dropzoneRender() {
-	$('body').html("<svg xmlns=\"http://www.w3.org/2000/svg\"xmlns:xlink=\"http://www.w3.org/1999/xlink\"></svg><div id=\"outer-dropzone\"><div id=\"inner-dropzone\" class=\"dropzone inline float-left\"></div><div id=\"word-text\" class=\"draggable drag-drop yes-drop\"></div>")
+	$('body').html("<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"></svg><div id=\"outer-dropzone\" class=\"dropzone\"><span id=\"sentence\" class=\"float-left\" ></span><div id=\"inner-dropzone\" class=\"dropzone\"></div></div><div id=\"word-text\" class=\"draggable drag-drop yes-drop\"></div>")
 }
+
+
 
