0f729d679e4f351419525395d382724eb9f1079f
Thuy <email": null>
2016-10-20 09:45:14 -0500

    remove empty SVG container

--- public/js/dropzone_render.js
+++ public/js/dropzone_render.js
@@ -1,5 +1,5 @@
 function dropzoneRender() {
-	$('body').html("<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"></svg><div id=\"outer-dropzone\" class=\"dropzone\"><span id=\"sentence\" class=\"float-left\" ></span><div id=\"inner-dropzone\" class=\"dropzone\"></div></div><div id=\"word-text\" class=\"draggable drag-drop yes-drop\"></div>")
+	$('body').html("<div id=\"outer-dropzone\" class=\"dropzone\"><span id=\"sentence\" class=\"float-left\" ></span><div id=\"inner-dropzone\" class=\"dropzone\"></div></div><div id=\"word-text\" class=\"draggable drag-drop yes-drop\"></div>")
 }
 
 
